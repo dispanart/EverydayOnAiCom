c119cf167ccaea560e30cced692f3e9955a88d8a
dispanart <86879580+dispanart@users.noreply.github.com>
2026-06-09 07:22:18 +0700

    Add GET route for ads.txt response

---
+++ app/ads.txt/route.js
@@ -0,0 +1,11 @@
+export async function GET() {
+  return new Response(
+    'google.com, pub-6538117569596224, DIRECT, f08c47fec0942fa0\n',
+    {
+      headers: {
+        'Content-Type': 'text/plain',
+        'Cache-Control': 'public, max-age=86400',
+      },
+    }
+  );
+}
