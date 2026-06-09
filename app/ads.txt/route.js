9b3a73f7d3ae9e085c86f6ad1df44b6cf048d521
dispanart <86879580+dispanart@users.noreply.github.com>
2026-06-09 12:33:53 +0700

    Enable dynamic routing for ads.txt

--- app/ads.txt/route.js
+++ app/ads.txt/route.js
@@ -1,7 +1,10 @@
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   return new Response(
     'google.com, pub-6538117569596224, DIRECT, f08c47fec0942fa0\n',
     {
+      status: 200,
       headers: { 'Content-Type': 'text/plain' },
     }
   );
